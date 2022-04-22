ca9f6a11489ac819d25f5d0e5b9f63eea556fac8
Melliowse <jayrizuri@gmail.com>
2022-04-22 02:39:28 -0400

    a

--- src/structures/interfaces/InteractionResponses.js
+++ src/structures/interfaces/InteractionResponses.js
@@ -39,7 +39,7 @@ class InteractionResponses {
 			files,
 		});
 		this.replied = true;
-		return this.client.actions.MessageCreate.handle(res).message;
+		return this.client.actions.MessageCreate.handle(res);
 	}
 
 	fetchReply() {
